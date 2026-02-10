8b585cb23b88ba34412d884c4cdee7fa64a667ce
cmathane2 <89760645+cmathane2@users.noreply.github.com>
2026-02-10 18:09:27 +0530

    Add method m11 to ClassNew

--- src/main/java/ClassNew.java
+++ src/main/java/ClassNew.java
@@ -33,4 +33,7 @@ public class ClassNew extends StringClass {
 		public void m10() {
 		System.out.println("code changes in master branch changes 1");
 	}
+		public void m11() {
+		System.out.println("code changes in master branch changes 1");
+	}
 }
