e72a5ff2c08b0bcec8a5f3cde07dbc3862221263
cmathane2 <89760645+cmathane2@users.noreply.github.com>
2026-02-15 23:09:32 +0530

    Update ClassNew.java

--- src/main/java/ClassNew.java
+++ src/main/java/ClassNew.java
@@ -49,4 +49,7 @@ public class ClassNew extends StringClass {
 	public void m18() {
 		System.out.println("Method done he cahnge ");
 	}
+		public void m20() {
+		System.out.println("Method done he cahnge ");
+	}
 }
